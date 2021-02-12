32a4c209f57cb1127df45db2af3a9b379a613fdb
Leo Velez <leonardo.velez@kaplan.edu>
2021-02-12 15:41:32 -0500

    portfolio page initial commit+uvg char sheet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portfolio';
+  printUVGSheet = false;
+  fatigue = ['grumpy', 'disadvan.', '1/2 speed', 'half-life', 'coma', 'death?'];
+  level = [
+    '0xp',
+    '300xp',
+    '750xp',
+    '1500xp',
+    '3000xp',
+    '6000xp',
+    '12500xp',
+    '25000xp',
+    '50000xp',
+    '99999xp',
+  ];
+  heroics = new Array(9);
+  inventory = new Array(20);
+
+  printUVG() {
+    this.printUVGSheet = true;
+    window.print();
+  }
 }
